e6f489037af704915dd2c21764557dd5e8a70762
Avanish Anand <avanish.anand@gmail.com>
2020-02-05 00:20:06 -0800

    Changes to validate a sentence

--- SentenceImpl/src/main/java/org/avanish/SentenceValidatorMainClass.java
+++ SentenceImpl/src/main/java/org/avanish/SentenceValidatorMainClass.java
@@ -1,4 +1,14 @@
 package org.avanish;
 
+import java.util.Scanner;
+
 public class SentenceValidatorMainClass {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String rawSentence = scanner.nextLine();
+        SentenceValidatorImpl sentenceValidator = new SentenceValidatorImpl();
+        System.out.println(sentenceValidator.isValidSentence(rawSentence));
+    }
 }
+
